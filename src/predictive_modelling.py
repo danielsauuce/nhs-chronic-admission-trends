58b861d65915c61f8fd09a2bf13c0393b44a7bac
Daniel <danieladeyd77@gmail.com>
2025-12-16 15:05:40 +0000

    data splitted into train and test

--- src/predictive_modelling.py
+++ src/predictive_modelling.py
@@ -9,3 +9,15 @@ from sklearn.model_selection import train_test_split
 # LOAD DATA
 england = pd.read_csv("../data/processed/england.csv")
 condition = pd.read_csv("../data/processed/condition.csv")
+
+# DATA PREPARATION
+england_model = england.dropna(subset=["year_start", "indicator_value"]).sort_values(
+    "year_start"
+)
+X_eng = england_model[["year_start"]]
+y_eng = england_model["indicator_value"]
+
+# Split for train/test validation
+X_train, X_test, y_train, y_test = train_test_split(
+    X_eng, y_eng, test_size=0.2, shuffle=False
+)
